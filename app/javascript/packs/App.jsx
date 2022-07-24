45a7657816e56e6845cf04b6dafcd39fc9655fd8
jacinyan <jacinjiyan@gmail.com>
2022-07-24 19:51:55 +1000

    Revert "Merge pull request #11 from jacinyan/jy-11"
    This reverts commit 909907b6b38580d90d2d81697ed36248db68806d, reversing
    changes made to 4fdc0b42c3ed956344d0f36e60a84f17f2041964.

--- app/javascript/packs/App.jsx
+++ app/javascript/packs/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/Home';
 import NotFound from './pages/NotFound';
-import avatar from '../../../public/avatar.jpeg';
 
 const App = () => {
   return (
